417c6957f58b9590c0f1b3396920b6aab6ffe56b
Jakob <business@jakkoble.de>
2023-09-27 11:23:28 +0200

    added redirect on 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+const NotFound = () => {
+  const router = useRouter();
+  useEffect(() => router.replace('/'), []);
+  return null;
+};
+
+export default NotFound;
