38d48bcdae4ef6e32d8a0bddddc080e0a41aa883
Jakob <business@jakkoble.de>
2023-09-28 10:43:13 +0200

    added rooms page

---
+++ app/rooms/page.tsx
@@ -0,0 +1,11 @@
+const Page = () => {
+  return (
+    <>
+      <h1 className="select-light dark:select-dark text-onBackground dark:text-dark-onBackground">
+        Räume
+      </h1>
+    </>
+  );
+};
+
+export default Page;
