1aad626b80ea59dbe76862a47461ba2172b64066
Jakob <business@jakkoble.de>
2023-10-05 16:01:38 +0200

    hide overflow

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="de">
-      <body className="text-white bg-background dark:bg-dark-background">
+      <body className="overflow-hidden bg-background dark:bg-dark-background">
         <main className="relative mx-4 min-h-screen md:mx-auto md:w-5/6 xl:w-[65%] 2xl:w-3/5">
           <ThemeProvider>
             <AuthProvider>
