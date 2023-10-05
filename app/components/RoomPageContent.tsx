9a3817f155c2cfd1095c9dc63bef73620920fe45
Jakob <business@jakkoble.de>
2023-10-05 11:33:57 +0200

    added desktop computer rendering

--- app/components/RoomPageContent.tsx
+++ app/components/RoomPageContent.tsx
@@ -1,11 +1,14 @@
-import { useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import Header from "./Header";
 import { useData } from "@/app/contexts/DataContext";
 import { useSearchParams } from "next/navigation";
 import Container from "./containers/Container";
+import Computer from "../types/computer";
 import ComputerDetail from "./ComputerDetail";
 
 const PageContent = () => {
+  const [leftComputers, setLeftComputers] = useState<Computer[]>([]);
+  const [rightComputers, setRightComputers] = useState<Computer[]>([]);
   const [loading, setLoading] = useState(true);
 
   const query = useSearchParams();
@@ -27,41 +30,93 @@ const PageContent = () => {
     [data.computers, room],
   );
 
+  const weightOfComputer = useCallback((computer: Computer) => {
+    const details = [
+      computer.ipAddress,
+      computer.macAddress,
+      computer.lastUser,
+    ];
+    const weight = details.filter((detail) => detail != null).length + 1;
+    return weight;
+  }, []);
+
+  var sortedComputers = computers?.sort(
+    (first, second) => weightOfComputer(second) - weightOfComputer(first),
+  );
+
+  console.log(leftComputers.length, rightComputers.length);
+
+  useEffect(() => {
+    var leftWeight = 0;
+    var rightWeight = 0;
+    const newLeftComputers: Computer[] = [];
+    const newRightComputers: Computer[] = [];
+
+    sortedComputers.forEach((computer) => {
+      if (leftWeight <= rightWeight) {
+        newLeftComputers.push(computer);
+        leftWeight += weightOfComputer(computer);
+      } else {
+        newRightComputers.push(computer);
+        rightWeight += weightOfComputer(computer);
+      }
+    });
+    setLeftComputers(newLeftComputers);
+    setRightComputers(newRightComputers);
+  }, [computers]);
+
+  const getComputerWidget = useCallback(
+    (computer: Computer, key: string) => (
+      <Container key={key}>
+        <h3 className="mb-1">{computer.name}</h3>
+        {computer.lastUser && (
+          <ComputerDetail
+            value={computer.lastUser}
+            iconPath="/user.svg"
+            altText="User Icon"
+          />
+        )}
+        {computer.ipAddress && (
+          <ComputerDetail
+            value={computer.ipAddress}
+            iconPath="/network.svg"
+            altText="IP-Address Icon"
+          />
+        )}
+        {computer.macAddress && (
+          <ComputerDetail
+            value={computer.macAddress}
+            iconPath="/computer.svg"
+            altText="Mac-Address Icon"
+          />
+        )}
+      </Container>
+    ),
+    [],
+  );
+
   return (
     <>
       <Header title={room.longName} previousPath="/rooms" />
-      <div className="grid w-full grid-cols-1 gap-4 sm:grid-cols-2">
-        {computers.length == 0 ? (
-          <h3>Keine Computer gefunden!</h3>
-        ) : (
-          computers.map((computer, index) => (
-            <Container key={index}>
-              <h3>{computer.name}</h3>
-              {computer.lastUser && (
-                <ComputerDetail
-                  value={computer.lastUser}
-                  iconPath="/user.svg"
-                  altText="User Icon"
-                />
+      {computers.length == 0 ? (
+        <h3>Keine Computer gefunden!</h3>
+      ) : (
+        <div className="w-full">
+          <div className="block sm:hidden"></div>
+          <div className="hidden w-full gap-4 sm:flex">
+            <div className="flex w-full flex-col gap-4">
+              {leftComputers.map((computer, index) =>
+                getComputerWidget(computer, `l-${index}`),
               )}
-              {computer.ipAddress && (
-                <ComputerDetail
-                  value={computer.ipAddress}
-                  iconPath="/network.svg"
-                  altText="IP Adress Icon"
-                />
+            </div>
+            <div className="flex w-full flex-col gap-4">
+              {rightComputers.map((computer, index) =>
+                getComputerWidget(computer, `r-${index}`),
               )}
-              {computer.macAddress && (
-                <ComputerDetail
-                  value={computer.macAddress}
-                  iconPath="/computer.svg"
-                  altText="Mac Adress Icon"
-                />
-              )}
-            </Container>
-          ))
-        )}
-      </div>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 };
