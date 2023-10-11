6e0e388f8f2f820b286a4c84ed2ebe24475d3dc5
Jakob <business@jakkoble.de>
2023-10-11 08:00:38 +0200

    added disabled option

--- app/components/containers/Container.tsx
+++ app/components/containers/Container.tsx
@@ -3,19 +3,22 @@ import Image from "next/image";
 interface ContainerWidgetProps {
   showArrow?: boolean;
   onClick?: () => void;
+  disabled?: boolean;
   children?: React.ReactNode;
 }
 
 const Container: React.FC<ContainerWidgetProps> = ({
   showArrow,
   onClick,
+  disabled,
   children,
 }) => {
   return (
     <div
       className={`w-full rounded-lg bg-secondary px-5 py-3 dark:bg-dark-secondary
     ${showArrow ? "flex items-center justify-between" : ""}
-    ${onClick ? "cursor-pointer" : ""}`}
+    ${onClick ? "cursor-pointer" : ""}
+    ${disabled ? "opacity-70" : ""}`}
       onClick={onClick}
     >
       {children}
