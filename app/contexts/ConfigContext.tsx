ac10715d1e359519d42f019eb5698a8cd5ad4787
Jakob <business@jakkoble.de>
2023-10-11 21:07:22 +0200

    uploaded latest

--- app/contexts/ConfigContext.tsx
+++ app/contexts/ConfigContext.tsx
@@ -1,11 +1,20 @@
 "use client";
 
-import { createContext, useCallback, useContext, useMemo, useRef } from "react";
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useMemo,
+  useRef,
+  useState,
+  useEffect,
+} from "react";
 import Config from "../types/config";
 
 type ConfigContextType = {
   config: Config | undefined;
   setConfig: (config: Config) => void;
+  hasUnsavedChanges: boolean;
   saveConfig: () => void;
 };
 
@@ -13,22 +22,31 @@ const ConfigContext = createContext<ConfigContextType | undefined>(undefined);
 
 export const ConfigProvider = ({ children }: { children: React.ReactNode }) => {
   const config = useRef<Config | undefined>(undefined);
+  const newConfig = useRef<Config | undefined>(undefined);
+
+  const [hasUnsavedChanges, setHasUnsavedChanges] = useState(false);
 
   const getConfig = useMemo(() => config.current, []);
 
-  const setConfig = useCallback((newConfig: Config) => {
-    config.current = newConfig;
+  const setConfig = useCallback((updatedConfig: Config) => {
+    newConfig.current = updatedConfig;
   }, []);
 
   const saveConfig = useCallback(() => {
     // TODO: Save config to Server
+    config.current = newConfig.current;
+    newConfig.current = undefined;
+    setHasUnsavedChanges(false);
   }, []);
+
+  useEffect(() => {});
 
   return (
     <ConfigContext.Provider
       value={{
         config: getConfig,
         setConfig,
+        hasUnsavedChanges,
         saveConfig,
       }}
     >
