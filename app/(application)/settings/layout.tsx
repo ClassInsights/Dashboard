c4c010c16488902548571bfdd746b5e5a162c75d
Jakob <business@jakkoble.de>
2023-10-11 08:40:14 +0200

    added settings page

---
+++ app/(application)/settings/layout.tsx
@@ -0,0 +1,9 @@
+import { ConfigProvider } from "@/app/contexts/ConfigContext";
+
+export default function SettingsLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <ConfigProvider>{children}</ConfigProvider>;
+}
---
+++ app/(application)/settings/page.tsx
@@ -0,0 +1,15 @@
+import Header from "@/app/components/Header";
+
+const SettingsPage = () => {
+  return (
+    <>
+      <Header
+        title="Einstellungen"
+        subtitle="Hier kannst du das ClassInsights Ökosystem bearbeiten."
+        previousPath="/"
+      />
+    </>
+  );
+};
+
+export default SettingsPage;
