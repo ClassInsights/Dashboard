f7b6b139eac1eaaab8011fe301714cf064e256a5
Jakob <business@jakkoble.de>
2023-10-17 18:20:30 +0200

    reworked config handling

--- app/contexts/ConfigContext.tsx
+++ app/contexts/ConfigContext.tsx
@@ -5,47 +5,70 @@ import {
   useCallback,
   useContext,
   useMemo,
-  useRef,
-  useState,
   useEffect,
+  useState,
 } from "react";
 import Config from "../types/config";
 
 type ConfigContextType = {
-  config: Config | undefined;
-  setConfig: (config: Config) => void;
+  getConfig: () => Config | undefined;
+  updateConfig: (updatedConfig: Config) => void;
   hasUnsavedChanges: boolean;
-  saveConfig: () => void;
+  saveConfig: () => Promise<boolean>;
 };
 
 const ConfigContext = createContext<ConfigContextType | undefined>(undefined);
 
 export const ConfigProvider = ({ children }: { children: React.ReactNode }) => {
-  const config = useRef<Config | undefined>(undefined);
-  const newConfig = useRef<Config | undefined>(undefined);
+  const [config, setConfig] = useState<Config | undefined>(undefined);
+  const [newConfig, setNewConfig] = useState<Config | undefined>(undefined);
 
-  const [hasUnsavedChanges, setHasUnsavedChanges] = useState(false);
+  const hasUnsavedChanges = useMemo(() => {
+    if (!config || !newConfig) return false;
+    return !(
+      config.teacherGroupId === newConfig.teacherGroupId &&
+      config.domainSid === newConfig.domainSid &&
+      config.caSubject === newConfig.caSubject &&
+      config.azureGroupPattern === newConfig.azureGroupPattern
+    );
+  }, [config, newConfig]);
 
-  const getConfig = useMemo(() => config.current, []);
+  const updateConfig = useCallback(
+    (updatedConfig: Config) => setNewConfig(updatedConfig),
+    [],
+  );
 
-  const setConfig = useCallback((updatedConfig: Config) => {
-    newConfig.current = updatedConfig;
-  }, []);
+  const getConfig = useCallback(() => newConfig ?? config, [newConfig, config]);
 
-  const saveConfig = useCallback(() => {
+  const saveConfig = useCallback(async () => {
     // TODO: Save config to Server
-    config.current = newConfig.current;
-    newConfig.current = undefined;
-    setHasUnsavedChanges(false);
-  }, []);
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    setConfig(newConfig);
+    setNewConfig(undefined);
+    return true;
+  }, [newConfig]);
 
-  useEffect(() => {});
+  useEffect(() => {
+    // TODO: Fetch config from Server
+    setConfig({
+      teacherGroupId: "e4816e17-4c37-8306-6fe6-9f96d74c4b25",
+      domainSid: "S-3-8-23-5892810687-2502163817-4270430042",
+      domainName: "projekt.local",
+      caSubject: "projekt-DC01PROJEKT-CA",
+      schoolYear: {
+        name: "2023/2024",
+        startDate: new Date(2023, 9, 10),
+        endDate: new Date(2024, 7, 6),
+      },
+      azureGroupPattern: "YEAR_CLASS",
+    });
+  }, []);
 
   return (
     <ConfigContext.Provider
       value={{
-        config: getConfig,
-        setConfig,
+        getConfig,
+        updateConfig,
         hasUnsavedChanges,
         saveConfig,
       }}
