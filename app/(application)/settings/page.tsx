c5032537321c6f3f4175c291c6a32b88ef7ee36d
Jakob <business@jakkoble.de>
2023-10-17 18:17:45 +0200

    use actual config data

--- app/(application)/settings/page.tsx
+++ app/(application)/settings/page.tsx
@@ -1,25 +1,36 @@
 "use client";
 
 import Header from "@/app/components/Header";
-import Container from "@/app/components/containers/Container";
 import ListContainer from "@/app/components/containers/ListContainer";
 import TextInput from "@/app/components/forms/TextInput";
+import { useConfig } from "@/app/contexts/ConfigContext";
+import { useMemo } from "react";
 
 const SettingsPage = () => {
+  const config = useConfig();
+
+  const currentConfig = useMemo(() => config.getConfig(), [config]);
+
   const details = [
     {
       name: "Aktuelles Jahr",
-      value: "2023/24",
+      value: config.getConfig()?.schoolYear.name ?? "Unbekannt",
     },
     {
       name: "Start",
-      value: "01.08.2023",
+      value:
+        config.getConfig()?.schoolYear.startDate.toLocaleDateString("de-AT") ??
+        "Unbekannt",
     },
     {
       name: "Ende",
-      value: "31.07.2024",
+      value:
+        config.getConfig()?.schoolYear.endDate.toLocaleDateString("de-AT") ??
+        "Unbekannt",
     },
   ];
+
+  if (!currentConfig) return;
 
   return (
     <>
@@ -28,7 +39,7 @@ const SettingsPage = () => {
         subtitle="Hier kannst du das ClassInsights Ökosystem bearbeiten."
         previousPath="/"
       />
-      <div className="flex select-none flex-col gap-4 sm:flex-row">
+      <section className="flex select-none flex-col gap-4 sm:flex-row">
         <ListContainer title="Schuljahr">
           {details.map((detail) => (
             <div className="flex select-none justify-between" key={detail.name}>
@@ -39,14 +50,41 @@ const SettingsPage = () => {
             </div>
           ))}
         </ListContainer>
-        <ListContainer title="Test">
-          <TextInput
-            onSubmit={(value) => console.log("Submitted", value)}
-            placeholder="Test"
-            style="tertiary"
-          />
-        </ListContainer>
-      </div>
+        <ListContainer title="Test"></ListContainer>
+      </section>
+      <section className="mt-10 flex w-full flex-col">
+        <div>
+          <h2 className="pb-2">Azure Gruppen Pattern</h2>
+          <div className="flex w-full items-start justify-between">
+            <div>
+              <p className="pb-2">
+                Hier kannst du das Muster/Pattern für die Azure Gruppen Namen
+                festlegen.
+              </p>
+              <p className="mb-0.5">Beispiele:</p>
+              <div className="grid w-max grid-cols-2 gap-x-10">
+                <small>Klasse</small>
+                <small className="mb-0.5">Pattern</small>
+                <p>2019_KK</p>
+                <p>YEAR_CLASS</p>
+                <p>KKB/2022</p>
+                <p>CLASS/YEAR</p>
+              </div>
+            </div>
+            <TextInput
+              initialValue={currentConfig.azureGroupPattern}
+              placeholder="Gruppen Pattern"
+              regexPattern="(?=.*YEAR)(?=.*CLASS)"
+              onSubmit={(value) =>
+                config.updateConfig({
+                  ...currentConfig,
+                  azureGroupPattern: value,
+                })
+              }
+            />
+          </div>
+        </div>
+      </section>
     </>
   );
 };
