0b935b74a997018b18773ad89bd3b0c4c3a0e486
Jakob <business@jakkoble.de>
2023-10-17 13:08:17 +0200

    added regex functionality

--- app/components/forms/TextInput.tsx
+++ app/components/forms/TextInput.tsx
@@ -1,23 +1,33 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 type TextInputProps = {
   value?: string;
   placeholder?: string;
+  style?: "primary" | "secondary" | "tertiary";
   regexPattern?: string;
-  maxLength?: number;
   onSubmit: (value: string) => void;
 };
 
 const TextInput: React.FC<TextInputProps> = ({
   value,
   placeholder,
+  style,
   regexPattern,
-  maxLength,
   onSubmit,
 }) => {
   const [inputValue, setInputValue] = useState(value ?? "");
+
+  const submitValue = useCallback(
+    (value: string) =>
+      regexPattern
+        ? new RegExp(regexPattern).test(value)
+          ? onSubmit(value)
+          : setInputValue(value)
+        : onSubmit(value),
+    [regexPattern, value, onSubmit],
+  );
 
   return (
     <input
@@ -25,8 +35,19 @@ const TextInput: React.FC<TextInputProps> = ({
       value={inputValue}
       onChange={(event) => setInputValue(event.target.value)}
       placeholder={placeholder}
-      className="outline-none"
-      onBlur={() => onSubmit(inputValue)}
+      className={`rounded-lg px-4 py-2 placeholder-onBackground outline-none placeholder:opacity-20 dark:placeholder-dark-onBackground
+      ${
+        style === "primary"
+          ? "bg-primary text-background dark:bg-dark-primary dark:text-dark-background"
+          : ""
+      }
+      ${
+        !style || style === "secondary"
+          ? "bg-secondary dark:bg-dark-secondary"
+          : ""
+      }
+      ${style === "tertiary" ? "bg-tertiary dark:bg-dark-tertiary" : ""}"}`}
+      onBlur={() => submitValue(inputValue)}
     />
   );
 };
