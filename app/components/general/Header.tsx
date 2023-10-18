67dc9dd3f2335304893ee8bfd823b652d0ea8ecc
Jakob <business@jakkoble.de>
2023-10-18 18:27:30 +0200

    adjusted reload action positioning

--- app/components/general/Header.tsx
+++ app/components/general/Header.tsx
@@ -18,8 +18,8 @@ const Header: React.FC<HeaderProps> = ({
 }) => {
   const router = useRouter();
   return (
-    <div className="mb-10 flex w-full select-none items-center justify-between gap-10">
-      <div className="w-full">
+    <div className="mb-10 select-none">
+      <div className="flex w-full items-center justify-between gap-10">
         <div
           className={`flex w-max ${
             previousPath !== undefined ? "cursor-pointer" : ""
@@ -40,23 +40,23 @@ const Header: React.FC<HeaderProps> = ({
           )}
           <h1>{title}</h1>
         </div>
-        {subtitle && <p className="mt-3 sm:w-3/4 lg:w-3/5">{subtitle}</p>}
+        {reloadAction && (
+          <div
+            className="dark:bg-primary-dark cursor-pointer rounded-md bg-primary p-1.5"
+            onClick={reloadAction}
+          >
+            <Image
+              src="/refresh.svg"
+              alt="Refresh rooms"
+              height={15}
+              width={15}
+              className="dark:onBackground-light onBackground-dark h-5 w-5"
+              draggable={false}
+            />
+          </div>
+        )}
       </div>
-      {reloadAction && (
-        <div
-          className="dark:bg-primary-dark flex-shrink-0 flex-grow cursor-pointer rounded-md bg-primary p-1.5"
-          onClick={reloadAction}
-        >
-          <Image
-            src="/refresh.svg"
-            alt="Refresh rooms"
-            height={15}
-            width={15}
-            className="dark:onBackground-light onBackground-dark h-5 w-5"
-            draggable={false}
-          />
-        </div>
-      )}
+      {subtitle && <p className="mt-3 sm:w-3/4 lg:w-3/5">{subtitle}</p>}
     </div>
   );
 };
