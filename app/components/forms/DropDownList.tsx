764d3e08a31f418e454f88364fc6546c7ebede54
Jakob <business@jakkoble.de>
2023-10-18 18:18:04 +0200

    added dynamic height and placeholder element

--- app/components/forms/DropDownList.tsx
+++ app/components/forms/DropDownList.tsx
@@ -1,5 +1,5 @@
 import { useState, useCallback } from "react";
-import Divider from "../settings/Divider";
+import Image from "next/image";
 
 type ListElement = {
   displayName?: string;
@@ -34,21 +34,40 @@ const DropDownList: React.FC<DropDownListProps> = ({
         className={`transition-opacity
         ${disabled ? "cursor-not-allowed opacity-50" : ""}`}
       >
-        <p
-          className={`bg-secondary px-4 py-2 dark:bg-dark-secondary ${
-            isOpen ? "rounded-t-md" : "rounded-md"
-          }`}
+        <div
+          className={`flex w-full items-center justify-between bg-secondary px-4 py-2 dark:bg-dark-secondary
+          ${isOpen ? "rounded-t-md" : "rounded-md"}`}
         >
-          {options.find((option) => option.value === selected)?.displayName ??
-            "Wähle aus"}
-        </p>
-        {isOpen && (
-          <div className="absolute z-30 w-full shadow-md">
-            {options.map((option, index) => (
+          <p>
+            {options.find((option) => option.value === selected)?.displayName ??
+              "Bitte wähle aus"}
+          </p>
+          <Image
+            src={"./arrow_right.svg"}
+            alt="arrow indicator for dropdown"
+            width={15}
+            height={15}
+            className={`h-4 w-4 transition-transform 
+            ${isOpen ? "-rotate-90" : "rotate-90"}`}
+            draggable={false}
+          />
+        </div>
+        {isOpen && options.length === 0 && (
+          <div className="absolute z-30 flex h-8 w-full items-center rounded-b-md border-t border-tertiary bg-secondary shadow-md dark:border-dark-tertiary dark:bg-dark-secondary">
+            <p className="px-4">Keine Elemente gefunden</p>
+          </div>
+        )}
+        {isOpen && options.length > 0 && (
+          <div
+            className={`absolute z-30 w-full overflow-y-scroll rounded-b-md shadow-md
+          ${
+            options.length <= 1 ? "h-8" : options.length === 2 ? "h-16" : "h-24"
+          }`}
+          >
+            {options.map((option) => (
               <p
                 key={option.value}
-                className={`cursor-pointer border-t border-tertiary bg-secondary px-4 py-2 transition-colors hover:bg-tertiary dark:border-dark-tertiary dark:bg-dark-secondary dark:hover:bg-dark-tertiary
-                ${index === options.length - 1 ? "rounded-b-md" : ""}`}
+                className="flex h-8 cursor-pointer items-center border-t border-tertiary bg-secondary px-4 transition-colors hover:bg-tertiary dark:border-dark-tertiary dark:bg-dark-secondary dark:hover:bg-dark-tertiary"
                 onClick={() => {
                   onChange(option);
                   setIsOpen(false);
