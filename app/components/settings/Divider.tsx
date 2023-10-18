b017e00ed561cd05a6d1a4afbf1f3efecfa120d6
Jakob <business@jakkoble.de>
2023-10-18 15:22:29 +0200

    added settings divider

---
+++ app/components/settings/Divider.tsx
@@ -0,0 +1,9 @@
+const Divider = () => {
+  return (
+    <div className="flex w-full justify-center">
+      <div className="my-12 h-0.5 w-11/12 bg-secondary opacity-40 dark:bg-dark-secondary" />
+    </div>
+  );
+};
+
+export default Divider;
