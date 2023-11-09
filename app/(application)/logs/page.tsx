23853530639690f722208c0f90bfd1e8f9984c6b
Jakob <business@jakkoble.de>
2023-11-09 21:29:34 +0100

    added log page

---
+++ app/(application)/logs/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/app/components/general/Header";
+
+export default function LogPage() {
+  return (
+    <>
+      <Header
+        title="Logbuch"
+        subtitle="Hier werden alle administrativen Tätigkeiten aufgezeichnet"
+        previousPath="/"
+      />
+    </>
+  );
+}
