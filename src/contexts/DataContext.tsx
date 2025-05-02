8caebaff3ea27fc8e5251189b45f5b95fa5a851d
Jakob <hey@jakob.foo>
2025-05-02 19:13:40 +0200

    fix: biome unsued method warning

--- src/contexts/DataContext.tsx
+++ src/contexts/DataContext.tsx
@@ -2,9 +2,7 @@ import { createContext, useCallback, useContext, useEffect, useState } from "rea
 import { isComputer, type Computer } from "../types/Computer";
 import { isRoom, type Room } from "../types/Room";
 import { useAuth } from "./AuthContext";
-import { isLesson, type Lesson } from "../types/Lesson";
-import { isSubject } from "../types/Subject";
-import { isClass } from "../types/Class";
+import type { Lesson } from "../types/Lesson";
 import { useToast } from "./ToastContext";
 
 export enum RoomSaveStatus {
@@ -35,7 +33,7 @@ const DataContext = createContext<DataContextType | undefined>(undefined);
 export const DataProvider = ({ children }: { children: React.ReactNode }) => {
 	const [computers, setComputers] = useState<Computer[] | undefined>(undefined);
 	const [rooms, setRooms] = useState<Room[] | undefined>(undefined);
-	const [lessons, setLessons] = useState<Lesson[] | undefined>(undefined);
+	const [lessons /* setLessons */] = useState<Lesson[] | undefined>(undefined);
 	const [isLoading, setIsLoading] = useState(true);
 	const [isRoomModalOpen, setIsRoomModalOpen] = useState(false);
 	const [isRefreshing, setIsRefreshing] = useState(false);
@@ -83,55 +81,55 @@ export const DataProvider = ({ children }: { children: React.ReactNode }) => {
 		return data.map((room) => (room.regex === "" ? { ...room, regex: null } : room));
 	}, [auth.data]);
 
-	const fetchLessons = useCallback(async () => {
-		// Fetch lessons
-		let response = await fetch(`${auth.data?.school.apiUrl}/lessons`, {
-			headers: {
-				Authorization: `Bearer ${auth.data?.accessToken}`,
-			},
-		});
-		if (!response.ok) throw new Error("Failed to fetch lessons");
-		const lessons = await response.json();
+	// const fetchLessons = useCallback(async () => {
+	// 	// Fetch lessons
+	// 	let response = await fetch(`${auth.data?.school.apiUrl}/lessons`, {
+	// 		headers: {
+	// 			Authorization: `Bearer ${auth.data?.accessToken}`,
+	// 		},
+	// 	});
+	// 	if (!response.ok) throw new Error("Failed to fetch lessons");
+	// 	const lessons = await response.json();
 
-		if (!Array.isArray(lessons) || !lessons.every((lesson) => isLesson(lesson)))
-			throw new Error(`Not a valid Lesson, ${JSON.stringify(lessons)}`);
+	// 	if (!Array.isArray(lessons) || !lessons.every((lesson) => isLesson(lesson)))
+	// 		throw new Error(`Not a valid Lesson, ${JSON.stringify(lessons)}`);
 
-		// Fetch subjects
-		response = await fetch(`${auth.data?.school.apiUrl}/subjects`, {
-			headers: {
-				Authorization: `Bearer ${auth.data?.accessToken}`,
-			},
-		});
+	// 	// Fetch subjects
+	// 	response = await fetch(`${auth.data?.school.apiUrl}/subjects`, {
+	// 		headers: {
+	// 			Authorization: `Bearer ${auth.data?.accessToken}`,
+	// 		},
+	// 	});
 
-		if (!response.ok) throw new Error("Failed to fetch lessons");
-		const subjects = await response.json();
-		if (!Array.isArray(subjects) || !subjects.every((subject) => isSubject(subject)))
-			throw new Error(`Not a valid Subject, ${JSON.stringify(subjects)}`);
+	// 	if (!response.ok) throw new Error("Failed to fetch lessons");
+	// 	const subjects = await response.json();
+	// 	if (!Array.isArray(subjects) || !subjects.every((subject) => isSubject(subject)))
+	// 		throw new Error(`Not a valid Subject, ${JSON.stringify(subjects)}`);
 
-		// Fetch classes
-		response = await fetch(`${auth.data?.school.apiUrl}/classes`, {
-			headers: {
-				Authorization: `Bearer ${auth.data?.accessToken}`,
-			},
-		});
+	// 	// Fetch classes
+	// 	response = await fetch(`${auth.data?.school.apiUrl}/classes`, {
+	// 		headers: {
+	// 			Authorization: `Bearer ${auth.data?.accessToken}`,
+	// 		},
+	// 	});
 
-		if (!response.ok) throw new Error("Failed to fetch lessons");
-		const classes = await response.json();
-		if (!Array.isArray(classes) || !classes.every((c) => isClass(c)))
-			throw new Error(`Not a valid Class, ${JSON.stringify(classes)}`);
+	// 	if (!response.ok) throw new Error("Failed to fetch lessons");
+	// 	const classes = await response.json();
+	// 	if (!Array.isArray(classes) || !classes.every((c) => isClass(c)))
+	// 		throw new Error(`Not a valid Class, ${JSON.stringify(classes)}`);
 
-		const finalLessons: Lesson[] = lessons.map((lesson) => {
-			const subject = subjects.find((subject) => subject.subjectId === lesson.subjectId);
-			const schoolClass = classes.find((c) => c.classId === lesson.classId);
-			return {
-				...lesson,
-				subject: subject?.displayName ?? "Unbekannt",
-				class: schoolClass?.displayName ?? "Unbekannt",
-			};
-		});
+	// 	const finalLessons: Lesson[] = lessons.map((lesson) => {
+	// 		const subject = subjects.find((subject) => subject.subjectId === lesson.subjectId);
+	// 		const schoolClass = classes.find((c) => c.classId === lesson.classId);
+	// 		return {
+	// 			...lesson,
+	// 			subject: subject?.displayName ?? "Unbekannt",
+	// 			class: schoolClass?.displayName ?? "Unbekannt",
+	// 		};
+	// 	});
 
-		return finalLessons;
-	}, [auth.data]);
+	// 	return finalLessons;
+	// }, [auth.data]);
 
 	const fetchData = useCallback(async () => {
 		let computers: Computer[];
