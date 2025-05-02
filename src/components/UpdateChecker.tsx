6bccf7e424fc5db9baa871744e758d538c11cac6
Jakob <hey@jakob.foo>
2025-05-02 19:04:05 +0200

    style: width on large screens

--- src/components/UpdateChecker.tsx
+++ src/components/UpdateChecker.tsx
@@ -7,27 +7,29 @@ const UpdateChecker = () => {
 	if (!update.updateAvailable) return null;
 
 	return (
-		<div className="page-spacing fixed right-0 bottom-0 left-0 z-50 w-full bg-container py-5">
+		<div className="fixed right-0 bottom-0 left-0 z-50 w-full bg-container py-5">
 			<CloseSVG
 				width={20}
 				className="absolute top-3 right-3 shrink-0 cursor-pointer fill-black"
 				onClick={() => update.abort()}
 			/>
-			<h3>Neue Version verfügbar!</h3>
-			{update.isUnix ? (
-				<>
-					<p>Klicke Sie auf Update, um ClassInsights automatisch auf den neusten Stand zu aktualisieren.</p>
-					<button
-						type="button"
-						onClick={() => update.callUpdate()}
-						className="mt-3 rounded-md bg-primary px-5 py-1 text-container"
-					>
-						Update
-					</button>
-				</>
-			) : (
-				<p>Bitte folgen Sie der Aktualisierungsanleitung.</p>
-			)}
+			<div className="page-spacing max-w-screen-xl">
+				<h3>Neue Version verfügbar!</h3>
+				{update.isUnix ? (
+					<>
+						<p>Klicke Sie auf Update, um ClassInsights automatisch auf den neusten Stand zu aktualisieren.</p>
+						<button
+							type="button"
+							onClick={() => update.callUpdate()}
+							className="mt-3 rounded-md bg-primary px-5 py-1 text-container"
+						>
+							Update
+						</button>
+					</>
+				) : (
+					<p>Bitte folgen Sie der Aktualisierungsanleitung.</p>
+				)}
+			</div>
 		</div>
 	);
 };
