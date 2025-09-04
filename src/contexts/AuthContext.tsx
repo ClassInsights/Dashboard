44f36b15d6219ce4982ba4c67a7e05667591fd2e
Jakkoble <hey@jakob.foo>
2025-09-04 20:31:37 +0200

    feat: added support link to auth error page

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -63,7 +63,7 @@ const AuthProvider = ({ children }: { children: React.ReactNode }) => {
         },
       );
 
-      if (!response.ok) throw new Error(`Failed to exchange token: ${response.status}`);
+      if (!response.ok) throw new Error(`Failed to exchange token, Status: ${response.status}`);
       const data = await response.json();
 
       if (!isTokenExchangeData(data)) throw new Error("Invalid token exchange data");
@@ -87,7 +87,7 @@ const AuthProvider = ({ children }: { children: React.ReactNode }) => {
         },
       });
 
-      if (!response.ok) throw new Error("Network response was not ok");
+      if (!response.ok) throw new Error(`Failed to get access token, Status: ${response.status}`);
       const data = await response.json();
 
       if (!isAccessTokenResponse(data)) throw new Error("Invalid access token response");
@@ -150,13 +150,27 @@ const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   if (tokenExchangeQuery.isError || accessTokenQuery.isError) {
     return (
       <div className="flex min-h-screen w-full flex-col items-center justify-center gap-12">
-        <img src="/logo.svg" alt="ClassInsights Logo" width={100} className="animate-pulse" />
+        <img src="/logo.svg" alt="ClassInsights Logo" width={100} />
         <div className="flex flex-col items-center pb-20 text-center">
           <h1 className="pb-6">Anmeldung fehlgeschlagen</h1>
           <p className="w-3/4 pb-5 lg:w-1/2">
             Leider war der Authentifizierungsvorgang nicht erfolgreich. Bitte versuchen Sie es in
             ein paar Minuten erneut. Wenn das Problem bestehen bleibt, melden Sie sich bitte bei
-            Ihrem Systemadministrator.
+            Ihrem Systemadministrator oder dem{" "}
+            <Button variant="link" className="m-0! p-0!">
+              <a
+                href={`mailto:office@classinsights.at?subject=Dashboard%20Crash&body=Bitte%20beschreiben%20Sie%20den%20Fehlerzeitpunkt%20und%20sonstige%20Besonderheiten%0A%0A%0A%0AINFORMATIONEN%20UND%20FEHLER%20NICHT%20ENTFERNEN%0A__________________________________________%0A%0AInformationen%3A%0ADashboard-Version:%20${import.meta.env.PACKAGE_VERSION}%0A%0AFehlerdetails%3A%0A${[
+                  tokenExchangeQuery.error,
+                  accessTokenQuery.error,
+                ]
+                  .map((e) => e?.message)
+                  .filter(Boolean)
+                  .join("%0A")}`}
+              >
+                ClassInsights Support
+              </a>
+            </Button>
+            . (Bitte die Schulinformationen und Fehlerdetails in der Email nicht entfernen!).
           </p>
           <Button onClick={logout}>Erneut versuchen</Button>
         </div>
