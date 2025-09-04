bf6ea113d54cac61d86ae354d8cecd2fc8dd2b64
Jakkoble <hey@jakob.foo>
2025-09-04 20:25:20 +0200

    refactor: make sure to include as many information in support mail as possible

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -9,11 +9,11 @@ const ErrorBoundaryFallback = ({ error }: FallbackProps) => {
         <h1 className="pb-6">Etwas ist fehlgeschlagen</h1>
         <p className="w-3/4 pb-5 lg:w-1/2">
           Leider ist beim Aufrufen des Dashboards ein unerwarteter Fehler aufgetreten. Bitte
-          versuchen Sie es später erneut. Wenn der Fehler bestehen bleibt kontaktieren Sie bitte
+          versuchen Sie es später erneut. Wenn der Fehler bestehen bleibt, kontaktieren Sie bitte
           Ihren Systemadministrator oder den{" "}
           <Button variant="link" className="m-0! p-0!">
             <a
-              href={`mailto:office@classinsights.at?subject=Dashboard%20Crash&body=%0A%0AFehlerdetails%3A%0A${error.message}`}
+              href={`mailto:office@classinsights.at?subject=Dashboard%20Crash&body=Bitte%20beschreiben%20Sie%20den%20Fehlerzeitpunkt%20und%20sonstige%20Besonderheiten%0A%0A%0A%0AINFORMATIONEN%20UND%20FEHLER%20NICHT%20ENTFERNEN%0A__________________________________________%0A%0AInformationen%3A%0ADashboard-Version:%20${import.meta.env.PACKAGE_VERSION}%0A%0AFehlerdetails%3A%0A${error.message}`}
             >
               ClassInsights Support
             </a>
--- src/components/Prefetcher.tsx
+++ src/components/Prefetcher.tsx
@@ -45,11 +45,11 @@ const Prefetcher = ({ children }: { children: React.ReactNode }) => {
           <h1 className="pb-6">Datenabfrage fehlgeschlagen</h1>
           <p className="w-3/4 pb-5 lg:w-1/2">
             Leider ist beim Abrufen der Daten ein unerwarteter Fehler aufgetreten. Bitte versuchen
-            Sie es später erneut. Wenn der Fehler bestehen bleibt kontaktieren Sie bitte Ihren
+            Sie es später erneut. Wenn der Fehler bestehen bleibt, kontaktieren Sie bitte Ihren
             Systemadministrator oder den{" "}
             <Button variant="link" className="m-0! p-0!">
               <a
-                href={`mailto:office@classinsights.at?subject=Dashboard%20Crash&body=%0A%0AINFORMATIONEN%20UND%20FEHLER%20NICHT%20ENTFERNEN%0A__________________________________________%0A%0AInformationen%3A%0ASchule: ${school.name}%0ASchul-ID: ${school.id}%0AAPI-Url: ${school.apiUrl}%0ADashboard-Url: ${school.dashboardUrl}%0ADashboard-Version: ${import.meta.env.PACKAGE_VERSION}%0A%0AFehlerdetails:%0A${[
+                href={`mailto:office@classinsights.at?subject=Dashboard%20Crash&body=Bitte%20beschreiben%20Sie%20den%20Fehlerzeitpunkt%20und%20sonstige%20Besonderheiten%0A%0A%0A%0AINFORMATIONEN%20UND%20FEHLER%20NICHT%20ENTFERNEN%0A__________________________________________%0A%0AInformationen%3A%0ASchule:%20${school.name}%0ASchul-ID:%20${school.id}%0AAPI-Url:%20${school.apiUrl}%0ADashboard-Url:%20${school.dashboardUrl}%0ADashboard-Version:%20${import.meta.env.PACKAGE_VERSION}%0A%0AFehlerdetails%3A%0A${[
                   computers.error,
                   rooms.error,
                   config.error,
