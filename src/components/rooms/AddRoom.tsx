9125c81fc289a9f2a81a35c27fe90d362077b517
Jakkoble <hey@jakob.foo>
2025-12-31 16:16:23 +0100

    typo: change OU to room

--- src/components/rooms/AddRoom.tsx
+++ src/components/rooms/AddRoom.tsx
@@ -70,7 +70,7 @@ const AddRoom = () => {
               </DialogDescription>
             </DialogHeader>
             <Command className="**:data-[slot=command-input-wrapper]:h-12 [&_[cmdk-group]]:px-2 [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
-              <CommandInput placeholder="Nach Organisationseinheit suchen..." />
+              <CommandInput placeholder="Nach Raum suchen..." />
               <CommandList>
                 <CommandEmpty>Keine Räume gefunden.</CommandEmpty>
                 {unassignedRooms?.map((room) => (
